25253c8ccc53d2f60771959b770e0c91e5eb34f4
Tomas Perez <tomaspz@gmail.com>
2020-04-30 19:02:24 -0400

    add the navbar as a shared component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Shared/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is my react app.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
     </div>
   );
 }
---
+++ src/Shared/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="/">Home</a>
+            <a class="navbar-brand" href="/directory">Directory</a>
+            <a class="navbar-brand" href="/details">Details</a>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </nav>
+    );
+};
+
+export default Navbar;
