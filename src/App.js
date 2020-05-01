6dba92b85f4ebed20cb1ec212529ac0e3f24f9ea
Tomas Perez <tomaspz@gmail.com>
2020-04-30 20:46:54 -0400

    reorganize file structure and file names. Add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
-import Navbar from './Shared/Navbar'
+import Navbar from './components/Shared/Navbar';
+import Directory from './containers/Directory';
+import Details from './containers/Details';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-
-    </div>
+      <Route exact path="/" component={Directory} />
+      <Route exact path="/directory" component={Directory} />
+      <Route exact path="/details" component={Details} />
+    </Router>
   );
 }
 
