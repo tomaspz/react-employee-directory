312a3ee05cec1e1cf3306466823edb0cba44eda9
Tomas Perez <tomaspz@gmail.com>
2020-05-02 00:02:50 -0400

    work on sorting columns

--- src/components/ListItems/Table.jsx
+++ src/components/ListItems/Table.jsx
@@ -4,6 +4,12 @@ import { Link } from 'react-router-dom';
 // import { useHistory } from "react-router-dom";
 
 const Table = (props) => {
+
+  // function getSortedData(data, prop, isAsc) {
+  //   return data.sort((a, b) => {
+  //       return (a[prop] < b[prop] ? -1 : 1) * (isAsc ? 1 : -1)
+  //   });
+  // }
   
   return (
     <div className="container">
--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -4,7 +4,7 @@ import '../logo.svg';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-dark bg-dark">
-            <a className="navbar-brand" href="/"><img src="logo.svg" alt="logo" style={{width: 50}}/></a>
+            <a className="navbar-brand" href="/"><img src="logo.svg" alt="logo" style={{width: 75}}/></a>
             <a className="navbar-brand" href="/">Directory</a>
             <a className="navbar-brand" href="/details">Details</a>
         </nav>
