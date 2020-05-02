692761ca3e3f09083dabf33fd4a28ed47cb5bf53
Tomas Perez <tomaspz@gmail.com>
2020-05-02 09:03:42 -0400

    Updates

---
+++ precache-manifest.8a467a06e5eee48bdab8cb92acc9b781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf81b5715e6640b9b226000b105fce2",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "75522da7bd9deb5d2918",
+    "url": "/react-employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eed5b698763023c8bfc0",
+    "url": "/react-employee-directory/static/js/2.660d86e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-employee-directory/static/js/2.660d86e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75522da7bd9deb5d2918",
+    "url": "/react-employee-directory/static/js/main.96aafc9a.chunk.js"
+  },
+  {
+    "revision": "04244e7005e34b33cd29",
+    "url": "/react-employee-directory/static/js/runtime-main.a0c97ca5.js"
+  }
+]);
